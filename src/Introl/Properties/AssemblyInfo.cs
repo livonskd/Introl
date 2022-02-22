07ae1d6d632066a764f979d8de568e7863fae6f5
livonskiD <livonskid@gmail.com>
2022-02-22 16:29:00 +0300

    feat: add solution with empty projects

---
+++ src/Introl/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: XmlnsPrefix("https://github.com/livonskd/introl", "il")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
