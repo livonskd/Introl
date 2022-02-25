28b367bcc89dee25bcd8b036a9e175a46fff9036
livonskiD <livonskid@gmail.com>
2022-02-25 21:09:08 +0300

    feat: add CornerRadiusAssist

--- src/Introl/Properties/AssemblyInfo.cs
+++ src/Introl/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Markup;
 
 [assembly: XmlnsPrefix("https://github.com/livonskd/introl", "il")]
+[assembly: XmlnsDefinition("https://github.com/livonskd/introl", "Introl.Assists")]
 [assembly: XmlnsDefinition("https://github.com/livonskd/introl", "Introl.Theming")]
 
 [assembly: ComVisible(false)]
